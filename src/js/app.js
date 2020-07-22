18ce0e4f9f2bbe6320386bda7dfcf70d15986f71
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-22 07:36:19 +0300

    Add function to display restaurants from the api

--- src/js/app.js
+++ src/js/app.js
@@ -87,10 +87,55 @@ class UI {
                 if (img !== ''){
                     this.showRestaurant(img, name, address, aggregate_rating, cuisines, cost, menu_url, url)
                 }
-            })
-            
+            })  
         }
+    }
 
+    showRestaurant(img, name, address, aggregate_rating, cuisines, cost, menu_url, url){
+        const div = document.createElement('div');
+        div.classList.add('col-11', 'mx-auto', 'my-3', 'col-md-4');
+        div.innerHTML = `
+        <div class="card">
+         <div class="card">
+          <div class="row p-3">
+           <div class="col-5">
+            <img src="${img}" class="img-fluid img-thumbnail" alt="">
+           </div>
+           <div class="col-5 text-capitalize">
+            <h6 class="text-uppercase pt-2 redText">${name}</h6>
+            <p>${address}</p>
+           </div>
+           <div class="col-1">
+            <div class="badge badge-success">
+             ${aggregate_rating}
+            </div>
+           </div>
+          </div>
+          <hr>
+          <div class="row py-3 ml-1">
+           <div class="col-5 text-uppercase ">
+            <p>cuisines : </p>
+            <p>cost for two :</p>
+           </div>
+           <div class="col-7 text-uppercase">
+            <p>${cuisines}</p>
+            <p>${cost}</p>
+           </div>
+          </div>
+          <hr>
+          <div class="row text-center no-gutters pb-3">
+           <div class="col-6">
+            <a href="${menu_url}" target="_blank" class="btn redBtn  text-uppercase"><i class="fas fa-book"></i> menu</a>
+           </div>
+           <div class="col-6">
+            <a href="${url}" target="_blank" class="btn redBtn  text-uppercase"><i class="fas fa-book"></i>website</a>
+           </div>
+          </div>
+         </div>
+        
+        `;
+
+        this.restaurantList.appendChild(div);
     }
 }
 
