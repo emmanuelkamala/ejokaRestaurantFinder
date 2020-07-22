7946a7fcaf2f0aaa47b6e259c11fc69f4a195d7a
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-22 05:14:08 +0300

    Add categories to the app from the restaurant api

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,43 @@
 class Ejoka {
     constructor(){
-        this.api = 'eaf59a06684b739431331d13cc75060d';
-        this.headers = {
-            method: 'GET',
+        this.api = "eaf59a06684b739431331d13cc75060d";
+        this.header = {
+            method: "GET",
             headers: {
-                'user-key': this.api,
-                'Content-Type': 'application/json'
+                "user-key": this.api,
+                "Content-Type": "application/json"
             },
-            credentials: 'same-origin'
-        }
+            credentials: "same-origin"
+        };
+    }
+
+    async searchAPI(){
+        const categoryURL = `https://developers.zomato.com/api/v2.1/categories`;
+        
+        const categoryInfo = await fetch(categoryURL, this.header);
+        const categoryJSON = await categoryInfo.json();
+        const categories = await categoryJSON.categories;
+
+        return {
+            categories
+        };
     }
 }
 
 class UI {
+    constructor(){
+        this.loader = document.querySelector('.loader');
+        this.restaurantList = document.getElementById('restaurant-list');
+    }
 
+    addSelectOptions(categories){
+        const search = document.getElementById('searchCategory');
+        let output = `<option value='0' selected>Select category</option>`;
+        categories.forEach(category => {
+            output += `<option value='${category.categories.id}'>${category.categories.name}</option>`;
+        })
+        search.innerHTML = output;
+    }
 }
 
 (function(){
@@ -24,7 +48,7 @@ class UI {
     const ejoka = new Ejoka();
     const ui = new UI();
 
-    document.addEventListener('DOMContentLoaded', ()=>{
-
+    document.addEventListener('DOMContentLoaded', () => {
+        ejoka.searchAPI().then(data => ui.addSelectOptions(data.categories));  
     })
 })();
